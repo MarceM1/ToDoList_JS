c31da2ed699661732570806899c3f17ea99b017e
Marcelo Melogno <marcelomelogno1986@gmail.com>
2023-08-17 20:28:39 -0300

    HTML, CSS y JS del proyecto.  Implementacion de ScrollReveal

--- js/main.js
+++ js/main.js
@@ -1,1 +1,49 @@
-console.log('Hola Marce');
+const input = document.querySelector("input");
+const addBtn = document.querySelector("#addBtn");
+const ul = document.querySelector("ul");
+const empty = document.querySelector("#empty");
+
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const text = input.value;
+
+  if (text !== "") {
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+
+    p.textContent = text;
+
+    li.appendChild(p);
+    ul.appendChild(li);
+    li.appendChild(addDeleteBtn());
+
+		input.value = "";
+		empty.style.display = "none";
+
+  } else {
+    alert("Ingresa una tarea!");
+  }
+});
+
+function addDeleteBtn() {
+  const deleteBtn = document.createElement("buttom");
+
+  deleteBtn.textContent = "X";
+	deleteBtn.className ="btn-delete";
+
+	deleteBtn.addEventListener('click', (e) => {
+
+		const item = e.target.parentElement;
+
+		ul.removeChild(item);
+
+		const items = document.querySelectorAll("li");
+
+		if (items.length === 0) {
+			empty.style.display ="block";
+		}
+	});
+
+	return deleteBtn;
+}
---
+++ js/scrollrevealScrips.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal();
+
+sr.reveal('#mainContainer', {
+    duration: 4000,
+    origin: "bottom",
+    distance: "-1000px"
+});
+
+
+
+
